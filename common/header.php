28c1d9fb6fabf9901850109b785ee3bd2336c4d8
Katherine Lynch <katherly@upenn.edu>
2018-04-02 16:09:05 -0400

    Initial commit

---
+++ common/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="<?php echo get_html_lang(); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php if ( $description = option('description')): ?>
+    <meta name="description" content="<?php echo $description; ?>" />
+    <?php endif; ?>
+    <?php
+    if (isset($title)) {
+        $titleParts[] = strip_formatting($title);
+    }
+    $titleParts[] = option('site_title');
+    ?>
+    <title><?php echo implode(' &middot; ', $titleParts); ?></title>
+
+    <?php echo auto_discovery_link_tags(); ?>
+
+    <!-- Plugin Stuff -->
+
+    <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
+
+
+    <!-- Stylesheets -->
+    <?php
+    queue_css_file(array('iconfonts','style'));
+    queue_css_url('//fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic');
+    queue_css_url('https://fonts.googleapis.com/css?family=Gentium+Basic|Open+Sans');
+    echo head_css();
+
+    echo theme_header_background();
+    ?>
+
+    <!-- JavaScripts -->
+    <?php
+    queue_js_file('vendor/modernizr');
+    queue_js_file('vendor/selectivizr', 'javascripts', array('conditional' => '(gte IE 6)&(lte IE 8)'));
+    queue_js_file('vendor/respond');
+    queue_js_file('vendor/jquery-accessibleMegaMenu');
+    queue_js_file('globals');
+    queue_js_file('default');
+    echo head_js();
+    ?>
+</head>
+<?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
+    <a href="#content" id="skipnav"><?php echo __('Skip to main content'); ?></a>
+    <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
+
+        <header role="banner">
+            <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
+            <div id="site-title"><?php echo link_to_home_page(theme_logo()); ?></div>
+        </header>
+
+        <div id="wrap">
+            <div class="menu-button button">Menu</div>
+            <nav id="primary-nav" role="navigation">
+                <?php echo public_nav_main(array('role' => 'navigation')); ?>
+                <div id="search-container" role="search">
+                    <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>
+                    <?php echo search_form(array('show_advanced' => true)); ?>
+                    <?php else: ?>
+                    <?php echo search_form(); ?>
+                    <?php endif; ?>
+                </div>
+            </nav>
+            <div id="content" role="main" tabindex="-1">
+                <?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
